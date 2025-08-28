fa50542868a2ae86e8061244a60e981f8d92e708
Mobeen Qasim <mobeenqq@gmail.com>
2025-08-27 20:46:21 -0400

    added search

--- src/utils/table.js
+++ src/utils/table.js
@@ -1,6 +1,53 @@
 import deleteDataTable from "./delete.js";
 import updateDataTable from "./update.js";
 
+
+const renderTable = (transactionsArray) => {
+  const table = document.querySelector('.tableBody');
+  table.innerHTML = ""; // clear old rows
+
+  transactionsArray.forEach(obj => {
+    const row = document.createElement('tr');
+    row.dataset.id = obj.id;
+
+    const informationTitle = document.createElement('td');
+    const informationCategory = document.createElement('td');
+    const informationType = document.createElement('td');
+    const informationAmount = document.createElement('td');
+    const informationDate = document.createElement('td');
+    const informationAction = document.createElement('td');
+    const btnDiv = document.createElement('div');
+    btnDiv.classList.add('btnDiv');
+
+    informationTitle.innerText = obj.title;
+    informationCategory.innerText = obj.category;
+    informationAmount.innerText = `$${obj.ammount}`;
+    informationType.innerText = obj.type;
+    informationDate.innerText = obj.date;
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("btn", "modal__btn-edit");
+    editBtn.innerText = "Edit";
+
+    const deleteBtns = document.createElement("button");
+    deleteBtns.classList.add("btn", "modal__btn-delete");
+    deleteBtns.innerText = "Delete";
+
+    btnDiv.appendChild(editBtn);
+    btnDiv.appendChild(deleteBtns);
+    informationAction.appendChild(btnDiv);
+
+    row.append(informationTitle, informationType, informationCategory, informationAmount, informationDate, informationAction);
+    table.appendChild(row);
+
+    deleteBtns.addEventListener('click', () => deleteDataTable(obj.id));
+    editBtn.addEventListener('click', () => updateDataTable(obj.id));
+  });
+};
+
+
+
+
 const tableGenerator = () => {
 
   const transactions = JSON.parse(localStorage.getItem('transactions') || '[]');
@@ -8,6 +55,20 @@ const tableGenerator = () => {
   const table = document.querySelector('.tableBody');
 
   table.innerHTML = "";
+
+
+  const searchInput = document.querySelector('#search');
+
+
+  searchInput.addEventListener('input', (e) => {
+    const query = searchInput.value.toLowerCase();
+
+    const filtered = transactions.filter(t =>
+      t.title.toLowerCase().includes(query) 
+    )
+
+   renderTable(filtered); 
+  })
   
   transactions.forEach(obj => {
     const row = document.createElement('tr');
@@ -55,6 +116,8 @@ const tableGenerator = () => {
       deleteDataTable(id);
     });
   });
+
+
 
 
  table.addEventListener("click", (e) => {
